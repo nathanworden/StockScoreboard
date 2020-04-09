386f1ddb1ccd27f4015c2f86cde7ee0a0c55fd28
Nathan Worden <worden.nathan@gmail.com>
2020-04-08 18:39:09 -0700

    sql setup file

--- data/sql_setup.sql
+++ data/sql_setup.sql
@@ -7,8 +7,8 @@ purchase_price decimal(9, 2) NOT NULL,
 comission decimal (6, 2) NOT NULL
 );
 
-INSERT INTO stocks (ticker, shares, purchase_date, purchase_price, comission)
-VALUES('amzn', 1, TO_DATE('2020/03/12', 'YYYY/MM/DD'), 1723, 7);
+-- INSERT INTO stocks (ticker, shares, purchase_date, purchase_price, comission)
+-- VALUES('amzn', 1, TO_DATE('2020/03/12', 'YYYY/MM/DD'), 1723, 7);
 
 CREATE TABLE s_and_p (
 id serial UNIQUE NOT NULL,
